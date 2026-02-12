fe6a73877985b65fd9e4864afd8ff9c4291a1ad8
BkStyle89 <kajzer@hotmail.it>
2026-02-12 17:39:16 +0100

    ticket train

--- Script/script.js
+++ Script/script.js
@@ -5,27 +5,24 @@ const ageEl= document.getElementById("age")
 const formEl = document.querySelector(".first")
 const createEl=document.getElementById("create")
 const cancelEl=document.getElementById("cancel")
-const U18El= document.getElementById("U18")
-const O18El= document.getElementById("O18")
-const O65El= document.getElementById("O65")
 console.log(nameEl,distanceEl,ageEl,formEl)
 
-/* click button function */
-createEl.addEventListener("click",function(){
-    console.log(nameEl.value,distanceEl.value,ageEl.value);
 
-})
 /* save info function */
 formEl.addEventListener("submit",function(e){
     e.preventDefault()
-    let price = distanceEl * 0.21;
+  /*   console.log(distanceEl.value) */
+    let price = distanceEl.value * 0.21;
     
-    if (ageEl === U18El){
-        price = price * 0.8
+    if (ageEl.value === "under18"){
+        price = price * 0.8;
+    }
+    else if (ageEl.value==="over65"){
+        price = price * 0.6;
     }
     console.log("costo finale biglietto è:" + " " + price.toFixed(2));
+    console.log(nameEl.value,distanceEl.value,ageEl.value )
 })
-  console.log(nameEl.value,distanceEl.value,ageEl.value )
   
   /* prezzo in base all'età biglietto */
   /* età passeggero tra maggiorenne over 65*/
