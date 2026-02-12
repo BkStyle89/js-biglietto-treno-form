c0cf11f75109c0ebb584d23ce3ee88e3684eb8e7
BkStyle89 <kajzer@hotmail.it>
2026-02-12 16:48:23 +0100

    Ja Click Function

--- Script/script.js
+++ Script/script.js
@@ -5,3 +5,14 @@ const formEl = document.querySelector(".first")
 const createEl=document.getElementById("create")
 const cancelEl=document.getElementById("cancel")
 console.log(nameEl,distanceEl,ageEl,formEl)
+
+
+createEl.addEventListener("click",function(){
+    console.log(nameEl.value,distanceEl.value,ageEl.value);
+
+})
+formEl.addEventListener("submit",function(e){
+    e.preventDefault()
+    console.log(nameEl.value,distanceEl.value,ageEl.value )
+})
+  console.log(nameEl.value,distanceEl.value,ageEl.value )
